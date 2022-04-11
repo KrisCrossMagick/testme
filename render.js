e4069c249854a6934a15487d3fbab298638192fb
KrisCrossMagick <github@knowhere.be>
2022-04-11 10:58:12 +0200

    finished the framework, fixed some bugs

--- render.js
+++ render.js
@@ -10,7 +10,14 @@ const render = async (filename) => {
 		resources  : 'usable'
 	});
 
-	return dom;
+	return new Promise((resolve, reject) => {
+		//this event will be triggered AFTER the DOM loads all the scripts
+		//this way we are sure our code will be ran before the tests
+		dom.window.document.addEventListener('DOMContentLoaded', () => {
+			//console.log('done loading');
+			resolve(dom); //dom will be returned when all scripts have been loaded
+		});
+	});
 };
 
 module.exports = render;
